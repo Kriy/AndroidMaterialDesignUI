3c9b66dc89edb3b5905f7f20d841f951e1f7cb7e
Kriy <hj2drf@163.com>
2017-07-28 22:39:59 +0800

    添加fba

--- app/src/main/java/com/materialdesignui/floatingactionbutton/FloatingActionButtonActivity.java
+++ app/src/main/java/com/materialdesignui/floatingactionbutton/FloatingActionButtonActivity.java
@@ -1,15 +1,169 @@
 package com.materialdesignui.floatingactionbutton;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.materialdesignui.R;
 
 public class FloatingActionButtonActivity extends AppCompatActivity {
 
+    private static final int DISTANCE = 300;
+    private static final int DISTANCE2 = 220;
+
+    private FloatingActionButton actionButton, actionButton1, actionButton2, actionButton3;
+    private boolean mMenuOpen = false;
+    private boolean mIsChange = false;
+    private View mFlMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floating_action_button);
+
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFlMenu.setVisibility(mIsChange ? View.VISIBLE : View.GONE);
+                mIsChange = !mIsChange;
+            }
+        });
+        initView();
+
+    }
+
+    private void initView() {
+        mFlMenu = findViewById(R.id.fl_menu);
+
+        actionButton = (FloatingActionButton) findViewById(R.id.float_btn);
+        actionButton1 = (FloatingActionButton) findViewById(R.id.float_btn1);
+        actionButton2 = (FloatingActionButton) findViewById(R.id.float_btn2);
+        actionButton3 = (FloatingActionButton) findViewById(R.id.float_btn3);
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMenuOpen) {
+                    hideMenu();
+                }else {
+                    showMenu();
+                }
+            }
+        });
+    }
+
+    private void showMenu() {
+        mMenuOpen = true;
+        int x = (int) actionButton.getX();
+        int y = (int) actionButton.getY();
+        ValueAnimator v1 = ValueAnimator.ofInt(x, x - DISTANCE);
+        v1.setDuration(500);
+        v1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int l = (int) animation.getAnimatedValue();
+                int t = (int) actionButton1.getY();
+                int r = actionButton1.getWidth() + l;
+                int b = actionButton1.getHeight() + t;
+                actionButton1.layout(l, t, r, b);
+            }
+        });
+        ValueAnimator v2x = ValueAnimator.ofInt(x, x - DISTANCE2);
+        ValueAnimator v2y = ValueAnimator.ofInt(y, y - DISTANCE2);
+        v2x.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int l = (int) animation.getAnimatedValue();
+                int t = (int) actionButton2.getY();
+                int r = actionButton2.getWidth() + l;
+                int b = actionButton2.getHeight() + t;
+                actionButton2.layout(l, t, r, b);
+            }
+        });
+        v2y.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int t = (int) animation.getAnimatedValue();
+                int l = (int) actionButton2.getX();
+                int r = actionButton2.getWidth() + l;
+                int b = actionButton2.getHeight() + t;
+                actionButton2.layout(l, t, r, b);
+            }
+        });
+        ValueAnimator v3 = ValueAnimator.ofInt(y, y - DISTANCE);
+        v3.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int t = (int) animation.getAnimatedValue();
+                int l = (int) actionButton3.getX();
+                int r = actionButton3.getWidth() + l;
+                int b = actionButton3.getHeight() + t;
+                actionButton3.layout(l, t, r, b);
+            }
+        });
+        v1.start();
+        v2x.start();
+        v2y.start();
+        v3.start();
+    }
+
+    private void hideMenu() {
+        mMenuOpen = false;
+        int x = (int) actionButton1.getX();
+        ValueAnimator v1 = ValueAnimator.ofInt(x, (int) actionButton.getX());
+        v1.setDuration(500);
+        v1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int l = (int) animation.getAnimatedValue();
+                int t = (int) actionButton1.getY();
+                int r = actionButton1.getWidth() + l;
+                int b = actionButton1.getHeight() + t;
+                actionButton1.layout(l, t, r, b);
+            }
+        });
+        x = (int) actionButton2.getX();
+        int y = (int) actionButton2.getY();
+        ValueAnimator v2x = ValueAnimator.ofInt(x, (int) actionButton.getX());
+        ValueAnimator v2y = ValueAnimator.ofInt(y, (int) actionButton.getY());
+        v2x.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int l = (int) animation.getAnimatedValue();
+                int t = (int) actionButton2.getY();
+                int r = actionButton2.getWidth() + l;
+                int b = actionButton2.getHeight() + t;
+                actionButton2.layout(l, t, r, b);
+            }
+        });
+        v2y.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int t = (int) animation.getAnimatedValue();
+                int l = (int) actionButton2.getX();
+                int r = actionButton2.getWidth() + l;
+                int b = actionButton2.getHeight() + t;
+                actionButton2.layout(l, t, r, b);
+            }
+        });
+        y = (int) actionButton3.getY();
+        ValueAnimator v3 = ValueAnimator.ofInt(y, (int) actionButton.getY());
+        v3.setDuration(500).addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int t = (int) animation.getAnimatedValue();
+                int l = (int) actionButton3.getX();
+                int r = actionButton3.getWidth() + l;
+                int b = actionButton3.getHeight() + t;
+                actionButton3.layout(l, t, r, b);
+            }
+        });
+        v1.start();
+        v2x.start();
+        v2y.start();
+        v3.start();
     }
 }
