4d52d7b93ad77938e7923723400c4fd7b1f2d182
Kriy <hj2drf@163.com>
2017-07-30 17:44:18 +0800

    add tablayout

---
+++ app/src/main/java/com/materialdesignui/tablayout/TabFragment.java
@@ -0,0 +1,10 @@
+package com.materialdesignui.tablayout;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by DF on 2017/7/30.
+ */
+
+public class TabFragment extends Fragment {
+}
--- app/src/main/java/com/materialdesignui/tablayout/TabLayoutActivity.java
+++ app/src/main/java/com/materialdesignui/tablayout/TabLayoutActivity.java
@@ -1,15 +1,36 @@
 package com.materialdesignui.tablayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.materialdesignui.R;
 
-public class TabLayoutActivity extends AppCompatActivity {
+public class TabLayoutActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_layout);
+
+        findViewById(R.id.btn_tab_01).setOnClickListener(this);
+        findViewById(R.id.btn_tab_02).setOnClickListener(this);
+        findViewById(R.id.btn_tab_03).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_tab_01:
+                break;
+            case R.id.btn_tab_02:
+                break;
+            case R.id.btn_tab_03:
+                break;
+        }
     }
 }
